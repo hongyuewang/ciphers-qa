e5aa1ac264fc1a9ce3a908e20257ed2a3bb086ba
Hong Yue Wang <hongyuew26@gmail.com>
2022-07-04 16:09:25 -0400

    Add caesar cipher

---
+++ src/main/java/cipher/CaesarCipher.java
@@ -0,0 +1,35 @@
+package cipher;
+
+public class CaesarCipher {
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encrypt(String message, int shiftKey) {
+        message = message.toLowerCase();
+        String cipherText = "";
+        for (int ii = 0; ii < message.length(); ii++) {
+            int charPosition = ALPHABET.indexOf(message.charAt(ii));
+            int keyVal = (shiftKey + charPosition) % 26;
+            if (keyVal < 0) {
+                keyVal = ALPHABET.length() + keyVal;
+            }
+            char replaceVal = ALPHABET.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return cipherText;
+    }
+
+    public static String decrypt(String cipherText, int shiftKey) {
+        cipherText = cipherText.toLowerCase();
+        String message = "";
+        for (int ii = 0; ii < cipherText.length(); ii++) {
+            int charPosition = ALPHABET.indexOf(cipherText.charAt(ii));
+            int keyVal = (charPosition - shiftKey) % 26;
+            if (keyVal < 0) {
+                keyVal = ALPHABET.length() + keyVal;
+            }
+            char replaceVal = ALPHABET.charAt(keyVal);
+            message += replaceVal;
+        }
+        return message;
+    }
+}
