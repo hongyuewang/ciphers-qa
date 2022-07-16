bb7dc0cc5f6f579ccba7f30d2502bf2f71a1fd96
Hong Yue Wang <hongyuew26@gmail.com>
2022-07-16 15:40:16 -0400

    Add all tests

--- src/test/java/cipher/CaesarCipherTest.java
+++ src/test/java/cipher/CaesarCipherTest.java
@@ -16,5 +16,35 @@ public class CaesarCipherTest {
         assertEquals( "mjqqt btwqi", CaesarCipher.encrypt(messageAlphabetOnly, 5));
         assertEquals( "dro iokb go vsfo sx, 2022, sc xyd k vokz iokb.", CaesarCipher.encrypt(messageDigitsAndSymbols, 10));
         assertEquals("hello world", CaesarCipher.encrypt(messageAlphabetOnly, 26));
+        assertEquals("hello world", CaesarCipher.encrypt(messageAlphabetOnly, 0));
+    }
+
+    @Test
+    public void encryptBackward() {
+        String messageAlphabetOnly = "Goodbye";
+        String messageDigitsAndSymbols = "123456789@! ;/";
+        assertEquals("bjjywtz", CaesarCipher.encrypt(messageAlphabetOnly, -5));
+        assertEquals("123456789@! ;/", CaesarCipher.encrypt(messageDigitsAndSymbols, -18));
+        assertEquals("goodbye", CaesarCipher.encrypt(messageAlphabetOnly, -26));
+    }
+
+    @Test
+    public void decryptForward() {
+        String cipherAlphabetOnly = "Mjqqt btwqi";
+        String cipherDigitsAndSymbols = "dro iokb go vsfo sx, 2022, sc xyd k vokz iokb.";
+        assertEquals("hello world", CaesarCipher.decrypt(cipherAlphabetOnly, 5));
+        assertEquals("the year we live in, 2022, is not a leap year.", CaesarCipher.decrypt(cipherDigitsAndSymbols, 10));
+        assertEquals("mjqqt btwqi", CaesarCipher.decrypt(cipherAlphabetOnly, 26));
+        assertEquals("mjqqt btwqi", CaesarCipher.decrypt(cipherAlphabetOnly, 0));
+    }
+
+    @Test
+    public void decryptBackward() {
+        String cipherAlphabetOnly = "Bjjywtz";
+        String cipherDigitsAndSymbols = "123456789@! ;/";
+
+        assertEquals("goodbye", CaesarCipher.decrypt(cipherAlphabetOnly, -5));
+        assertEquals("123456789@! ;/", CaesarCipher.decrypt(cipherDigitsAndSymbols, -18));
+        assertEquals("bjjywtz", CaesarCipher.encrypt(cipherAlphabetOnly, -26));
     }
 }
